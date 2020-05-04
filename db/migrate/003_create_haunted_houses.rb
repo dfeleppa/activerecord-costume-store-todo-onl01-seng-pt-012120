4cb0b78a359228e1e76aff72ca1b68195bfbd36c
Daniel J Feleppa <dfeleppa@gmail.com>
2020-05-04 12:14:23 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,8 @@
 # Create your haunted_houses migration here
+class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
+end
